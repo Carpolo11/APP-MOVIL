c13e39ea75edd02aed9d7e1cd9f8f77e673e48a4
Carlos Alegría <Ca9085079@gmail.com>
2025-09-28 17:28:50 -0500

    Proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <ion-app>
-    <ion-router-outlet />
-  </ion-app>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Hola desde Ionic + Vue 🚀</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <ion-button expand="block" color="primary">
+        Presióname
+      </ion-button>
+    </ion-content>
+  </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+<script setup>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton
+} from '@ionic/vue';
 </script>
+
